0378e62cbf974c6946abd9375bf0fa48f4ca5aa6
sunlei <sunlei@ihomefnt.com>
2021-06-10 21:37:05 +0800

    feat: 请求body处理

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -1,67 +1,82 @@
 import axios from '../../src/index'
 
-// ?foo[]=bar&foo[]=baz
-// res: foo: ["bar", "baz"]
-axios({
-  method: 'get',
-  url: '/base/get',
-  params: {
-    foo: ['bar', 'baz']
-  }
-})
+// // ?foo[]=bar&foo[]=baz
+// // res: foo: ["bar", "baz"]
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   params: {
+//     foo: ['bar', 'baz']
+//   }
+// })
 
-// ?foo=%7B%22bar%22:%22baz%22%7D
-// res: {"foo":"{\"bar\":\"baz\"}"}
-axios({
-  method: 'get',
-  url: '/base/get',
-  params: {
-    foo: {
-      bar: 'baz'
-    }
-  }
-})
+// // ?foo=%7B%22bar%22:%22baz%22%7D
+// // res: {"foo":"{\"bar\":\"baz\"}"}
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   params: {
+//     foo: {
+//       bar: 'baz'
+//     }
+//   }
+// })
 
-// TODO
-// ?date=2021-06-09T20:46:55
-const date = new Date()
-axios({
-  method: 'get',
-  url: '/base/get',
-  params: {
-    date
-  }
-})
+// // TODO
+// // ?date=2021-06-09T20:46:55
+// const date = new Date()
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   params: {
+//     date
+//   }
+// })
+
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   params: {
+//     foo: '@:$, '
+//   }
+// })
+
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   params: {
+//     foo: 'bar',
+//     baz: null
+//   }
+// })
+
+// axios({
+//   method: 'get',
+//   url: '/base/get#hash',
+//   params: {
+//     foo: 'bar'
+//   }
+// })
+
+// axios({
+//   method: 'get',
+//   url: '/base/get?foo=bar',
+//   params: {
+//     bar: 'baz'
+//   }
+// })
 
 axios({
-  method: 'get',
-  url: '/base/get',
-  params: {
-    foo: '@:$, '
-  }
-})
-
-axios({
-  method: 'get',
-  url: '/base/get',
-  params: {
+  method: 'post',
+  url: '/base/post',
+  data: {
     foo: 'bar',
-    baz: null
-  }
-})
-
-axios({
-  method: 'get',
-  url: '/base/get#hash',
-  params: {
-    foo: 'bar'
-  }
-})
-
-axios({
-  method: 'get',
-  url: '/base/get?foo=bar',
-  params: {
     bar: 'baz'
   }
+})
+
+axios({
+  method: 'post',
+  url: '/base/buffer',
+  data: new Int32Array([99990, 92])
 })
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { IAxiosRequestConfig } from './types'
 
 import xhr from './xhr'
 import { buildURL } from './helpers/url'
+import { isPlainObject } from './helpers/utils'
 
 export default function axios(config: IAxiosRequestConfig): any {
   console.log('axios request.......')
@@ -11,11 +12,26 @@ export default function axios(config: IAxiosRequestConfig): any {
   })
 }
 
-function processConfig(config: IAxiosRequestConfig) {
+function processConfig(config: IAxiosRequestConfig): void {
   config.url = transformURL(config)
+  config.data = transformRequestData(config)
 }
 
 function transformURL(config: IAxiosRequestConfig): string {
   const { url, params } = config
   return buildURL(url, params)
+}
+
+function transformRequestData(config: IAxiosRequestConfig): any {
+  const { method, data } = config
+
+  if (method?.toUpperCase() === 'GET' || method?.toUpperCase() === 'HEAD') {
+    return null
+  }
+
+  if (isPlainObject(data)) {
+    return JSON.stringify(data)
+  }
+
+  return data
 }
