e1667e567893b3ea677557b874fcb7305594b0ed
monking92 <qzsunlei@163.com>
2022-09-13 00:20:19 +0800

    feat: 复杂跨域 options请求

--- examples/more/app.ts
+++ examples/more/app.ts
@@ -10,5 +10,19 @@ axios({
 axios({
   url: 'http://localhost:7001/more/credentials',
   method: 'post',
+  withCredentials: true,
+  headers: {
+    'content-type': 'application/json',
+    'custome-header': 'sun'
+  },
+  data: {
+    method: 'post',
+    cors: true
+  }
+})
+
+axios({
+  url: 'http://localhost:7001/more/credentials',
+  method: 'get',
   withCredentials: true
 })
--- examples/server2.js
+++ examples/server2.js
@@ -1,27 +1,38 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 
 const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
 
-const crossOrigin = {
+const cors = {
   'Access-Control-Allow-Origin': 'http://localhost:8081',
-  'Access-Control-Allow-Methods': 'POST, OPTIONS',
+  // 'Access-Control-Allow-Methods': 'OPTIONS',
   'Access-Control-Allow-Credentials': true,
-  'Access-Control-Allow-Headers': 'Content-Type'
+  'Access-Control-Allow-Headers': 'Content-Type, custome-header'
 }
 
 const router = express.Router()
 
 router.post('/more/credentials', (req, res) => {
-  console.log('server2 req.cookies: ', req.cookies)
-  res.set(crossOrigin)
+  console.log('server2 post req.cookies: ', req.cookies)
+  console.log('server2 post req.body: ', req.body)
+  res.set(cors)
   res.json(req.cookies)
 })
 
 router.options('/more/credentials', (req, res) => {
-  res.set(crossOrigin)
+  console.log('server2 options req.cookies: ', req.cookies)
+  console.log('server2 options req.body: ', req.body)
+  res.set(cors)
   res.end()
+})
+
+router.get('/more/credentials', (req, res) => {
+  res.set(cors)
+  res.end('cors get')
 })
 
 app.use(router)
